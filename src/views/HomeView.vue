5e54bae19d7493e7028f8546d7f6506a7161906c
Aaron Alley <aaronalleydev@gmail.com>
2022-05-03 13:30:10 +0000

    simple jobs board

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum id dolor amet perspiciatis reprehenderit debitis dolores, assumenda deleniti magnam qui sit quidem repudiandae, beatae officia facere accusamus excepturi ad officiis sapiente praesentium? Ratione nostrum accusamus odit ullam autem eligendi adipisci quia minus voluptatibus repellat odio tenetur laborum, quam natus veritatis perferendis eius consequatur explicabo soluta magnam optio saepe? Dicta aliquid eius eligendi veritatis itaque numquam odit laudantium qui, voluptatibus ad voluptas sequi delectus cupiditate rerum architecto quam cumque repellendus ut voluptates nam incidunt dolorem commodi nulla natus. Corrupti nesciunt excepturi laboriosam eveniet, eligendi, nostrum tenetur aspernatur ipsum velit, eius debitis?
+    </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum id dolor amet perspiciatis reprehenderit debitis dolores, assumenda deleniti magnam qui sit quidem repudiandae, beatae officia facere accusamus excepturi ad officiis sapiente praesentium? Ratione nostrum accusamus odit ullam autem eligendi adipisci quia minus voluptatibus repellat odio tenetur laborum, quam natus veritatis perferendis eius consequatur explicabo soluta magnam optio saepe? Dicta aliquid eius eligendi veritatis itaque numquam odit laudantium qui, voluptatibus ad voluptas sequi delectus cupiditate rerum architecto quam cumque repellendus ut voluptates nam incidunt dolorem commodi nulla natus. Corrupti nesciunt excepturi laboriosam eveniet, eligendi, nostrum tenetur aspernatur ipsum velit, eius debitis?
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
